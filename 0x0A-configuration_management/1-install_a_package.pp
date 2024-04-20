5271928537f0d9ed1dbaeabffbcd15231c92037a
BAHIIM <abde2001raja@gmail.com>
2024-04-20 02:52:18 -0700

    wd

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/pup3
-# Install flask
-package { 'flask':
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3'
 }
-
